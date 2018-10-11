f327a865f5fc9e6df92edd7b514e777f1610b797
Joshua Tucker <thejltucker@gmail.com>
2018-10-11 15:07:01 -0400

    04E2

---
+++ 04_Encapsulation_2/Room.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Encapsulation_2
+{
+    public class Room
+    {
+        private const double MAX_LENGTH = 30;
+        private const double MIN_LENGTH = 6;
+
+        private const double MAX_WIDTH = 30;
+        private const double MIN_WIDTH = 6;
+
+        private const double MAX_HEIGHT = 12;
+        private const double MIN_HEIGHT = 10;
+
+        private double _length;
+        private double _width;
+        private double _height;
+
+        public Room(double length, double width, double height)
+        {
+            Length = length;
+            Width = width;
+            Height = height;
+        }
+
+        public double Length
+        {
+            get => _length;
+            private set
+            {
+                if(value<MIN_LENGTH || value > MAX_LENGTH)
+                {
+                    throw new ArgumentException($"The length should be between {MIN_LENGTH} and {MAX_LENGTH}.");
+                }
+
+                _length = value;
+            }
+        }
+
+        public double Width
+        {
+            get => _width;
+            private set
+            {
+                if (value < MIN_WIDTH || value > MAX_WIDTH)
+                {
+                    throw new ArgumentException($"The width should be between {MIN_WIDTH} and {MAX_WIDTH}.");
+                }
+
+                _width = value;
+            }
+        }
+
+        public double Height
+        {
+            get => _height;
+            private set
+            {
+                if (value < MIN_HEIGHT || value > MAX_HEIGHT)
+                {
+                    throw new ArgumentException($"The height should be between {MIN_HEIGHT} and {MAX_HEIGHT}.");
+                }
+
+                _height = value;
+            }
+        }
+
+        public double CalculateLateralSurfaceArea()
+        {
+            return 2 * _length * _height + 2 * _width * _height;
+        }
+    }
+}
---
+++ 04_Encapsulation_2_Unit_Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("04_Encapsulation_2_Unit_Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("04_Encapsulation_2_Unit_Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("eb0e98af-1e10-4d63-9801-39de9fa8193c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
